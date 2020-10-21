7d0e9dccb26ebc831b244c6f58466f76b1804acc
Pete Vlahos <pgvlahos@gmail.com>
2020-10-21 08:59:46 -0700

    Solved Diff Month to Mont on revenue

--- pybank/main.py
+++ pybank/main.py
@@ -8,6 +8,8 @@ csvpath = os.path.join('Resources','budget_data.csv')
 #create 2 list 1 for dates 2 for revenue
 dates = []
 revenue = []
+profit_loss = []
+net_sum_p_l = 0
 
 # open with path variable, new line data 
 with open(csvpath, newline='') as csvfile:
@@ -21,11 +23,22 @@ with open(csvpath, newline='') as csvfile:
         #import pdb; pdb.set_trace()
             
         dates.append(row[0])
-
         revenue.append(float(row[1]))
 
         row_counter = row_counter + 1
     print(row_counter)
+
+    
+    for j in range(2,row_counter):
+
+        # net sum p/l
+
+        # Difference between rev j & rev j-1
+        profit_loss = (revenue[j] - revenue[j-1])
+        #print(profit_loss)
+
+        #
+
 
 # zipped 2 list, create a variable to hold list
 new_clean_file = zip(dates, revenue)
