f07d491119f75c6fe05d99b72b5f740d32e5b808
Pete Vlahos <pgvlahos@gmail.com>
2020-10-24 23:41:27 -0700

    Cleaned up with comments

--- pybank/revenue_analysis.py
+++ pybank/revenue_analysis.py
@@ -13,39 +13,42 @@ with open(csvpath, newline='') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
 
     header=next(csvreader)
+
     total_revenue_months = 0
     total_sum_revenue = 0
     total_change_in_revenue = 0
     average_change_in_revenue = 0
-    greatest_increase_rev = 0
-    greatest_decrease_rev = 0
    
     for row in csvreader:
 
         total_revenue_months += 1
-        
-        # FIX need to add back in 1st element outside loop
-        total_sum_revenue = total_sum_revenue + int(row[1])
+
+        #lists created from csv reader
 
         date_list.append(row[0])
         revenue_list.append(int(row[1]))
 
+#Creat function to find sums in lists (FIX need to figure out how to use a loop)
 def sum_list (list):
     sum = 0
     for i in list:
         sum += i
     return sum
 
+# Total revenue using def sum_list function
 total_sum_revenue = sum_list(revenue_list)
 
+# Change in monthly revenue list  
 for i in range(1,total_revenue_months):
     change_in_revenue_list.append(revenue_list[i] - revenue_list[i-1])
 
+#total change in revenue to find average change
 total_change_in_revenue = sum_list(change_in_revenue_list)
 
+#average change in rev using def function
 average_change_in_revenue = round(total_change_in_revenue / (total_revenue_months-1),2)
 
-
+#pop 1st row to zip equal elements in date/change rev list
 date_list.pop(0)
 date_change_rev_list = [I for I in zip(date_list, change_in_revenue_list)]
 
